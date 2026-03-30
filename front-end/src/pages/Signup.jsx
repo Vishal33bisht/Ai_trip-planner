6a67bf1a658b0f16a1eb0bb4392370bf3987fc47
vishal <bishtv773@gmail.com>
2026-03-30 11:25:44 +0530

    chanegd the error message

--- front-end/src/pages/Signup.jsx
+++ front-end/src/pages/Signup.jsx
@@ -36,7 +36,7 @@ const Signup = () => {
         setError(data.detail || "Signup failed. Email may already exist.");
       }
     } catch (err) {
-      setError("Connection error. Please try again.");
+      setError("apkiii g me dandaaa");
     } finally {
       setLoading(false);
     }
