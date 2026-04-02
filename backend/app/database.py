50dc32ba6ab549b0db6a871565e994fae8fadbca
vishal <bishtv773@gmail.com>
2026-04-02 09:43:10 +0530

    changed message

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in .env")
+    raise ValueError("DATABASE_URL is not set")
 
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
